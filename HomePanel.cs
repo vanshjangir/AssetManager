71e30ff91d5d73f10f53cf7d31a19296892f8a6a
vansh <vanshjangir0001@gmail.com>
2023-11-26 18:07:45 +0530

    image downloading

--- HomePanel.cs
+++ HomePanel.cs
@@ -1,24 +1,32 @@
 #if TOOLS
 using Godot;
 using System;
+using System.Net;
 using System.Collections.Generic;
 
 [Tool]
 public partial class HomePanel : Panel
 {
 	
-	
-	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		Button itch = GetNode<Button>("PanelContainer/VBoxContainer/HBoxContainer/itch_button");
 		var itchCallable = new Callable(this, nameof(onItchButtonPressed));
 		itch.Connect("pressed", itchCallable);
 	}
-
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
+	
+	public void imageLoad(string imageUrl, string localPath){
+		using (WebClient webClient = new WebClient())
+		{
+			try
+			{
+				webClient.DownloadFile(imageUrl, localPath);
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine($"Error downloading image: {ex.Message}");
+			}
+		}
 	}
 	
 	private void onItchButtonPressed()
@@ -26,19 +34,23 @@ public partial class HomePanel : Panel
 		var yamateSound = GetNode<AudioStreamPlayer>("AudioStreamPlayer");
 		yamateSound.Stream = GD.Load<AudioStream>("res://addons/AssetManager/button.wav");
 		yamateSound.Play((float)0.03);
+		
 		Assets assetsScraper = new Assets();
 		List<Dictionary<string, string>> assetList = assetsScraper.Load();
+		int count = 1;
 		foreach (var assetData in assetList)
-	{
-		string imageUrl = assetData["image"];
-		string imageText = assetData["text"];
-		string imageLink = assetData["link"];
-		GD.Print($"Image URL: {imageUrl}");
-		GD.Print($"Asset Text: {imageText}");
-		GD.Print($"Asset Link: {imageLink}");
-		GD.Print();
-	}
-
+		{
+			string localPath = $@"E:\vansh\GodotTest\addons\AssetManager\tmp\image{count}.png";
+			string imageUrl = assetData["image"];
+			string imageText = assetData["text"];
+			string imageLink = assetData["link"];
+			GD.Print($"Image URL: {imageUrl}");
+			GD.Print($"Asset Text: {imageText}");
+			GD.Print($"Asset Link: {imageLink}");
+			GD.Print();
+			imageLoad(imageUrl, localPath);
+			count++;
+		}
 	}
 }
 #endif
